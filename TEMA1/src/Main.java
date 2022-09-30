3167def0c6de33cbe72829526d1aeb70aa4b736f
unknown <Xavi Gallego Palau>
2022-09-30 12:07:38 +0200

    Primer ejercicio

---
+++ TEMA1/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int entero = 626;
+        long largo = 1233233343;
+        double doble = 21.34;
+        boolean prueba = false;
+        boolean prueba2 = true;
+        String imprimo = "Se a imprimido";
+
+        System.out.println(entero);
+        System.out.println(largo);
+        System.out.println(doble);
+        System.out.println(prueba);
+        System.out.println(prueba2);
+        System.out.println(imprimo);
+    }
+}
